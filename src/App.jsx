3213781b7916c891daaacda2ef5a70b69a6b0523
Peter M <pmoyano@solvd.com>
2023-01-31 15:27:06 -0300

    useRef to access a DOM node

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <Form />
-      <h1>This is from App.jsx</h1>
     </div>
   );
 }
--- src/reference/useRef/Form.jsx
+++ src/reference/useRef/Form.jsx
@@ -1,4 +1,19 @@
-import React from "react";
+import { useRef } from "react";
+
+// This is an expample of using the useRef hook to access a DOM node. In this case an input.
+// As always the inputRef is set outside the rendering cycle of the Form component (use a useEffect or event handler to do so).
+// now I can access the dom Node with all of its methods, including focus()
 export default function Form() {
-  return <h1>This is from Form component</h1>;
+  const inputRef = useRef(null);
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
+
+  return (
+    <>
+      <input ref={inputRef} />
+      <button onClick={handleClick}>Focus the input</button>
+    </>
+  );
 }
